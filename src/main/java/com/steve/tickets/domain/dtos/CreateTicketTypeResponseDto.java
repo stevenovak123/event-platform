52e7e5d9eadc285eeb3b09e1d3687744e90c81c7
stevenovak123 <stevenovak0802@gmail.com>
2025-07-13 13:25:44 +0530

    BugFix: Missed Description, added it

--- src/main/java/com/steve/tickets/domain/dtos/CreateTicketTypeResponseDto.java
+++ src/main/java/com/steve/tickets/domain/dtos/CreateTicketTypeResponseDto.java
@@ -15,6 +15,7 @@ public class CreateTicketTypeResponseDto {
     private UUID id;
     private String name;
     private Double price;
+    private String description;
     private Integer totalAvailable;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
