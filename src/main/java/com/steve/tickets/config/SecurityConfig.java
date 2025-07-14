0208df9afee9c7b9936dff6d298e88d5f4a0afd2
stevenovak123 <stevenovak0802@gmail.com>
2025-07-14 15:25:43 +0530

    Added Role based authorization

--- src/main/java/com/steve/tickets/config/SecurityConfig.java
+++ src/main/java/com/steve/tickets/config/SecurityConfig.java
@@ -13,10 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, UserProvisioningFilter userProvisioningFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, UserProvisioningFilter userProvisioningFilter, JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
         http.
-                authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll().anyRequest().authenticated()).
-                csrf(csrf -> csrf.disable()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).
+                authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll().requestMatchers("/api/v1/events").hasRole("ORGANIZER").anyRequest().authenticated()).
+                csrf(csrf -> csrf.disable()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter))).
                 addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
         return http.build();
     }
